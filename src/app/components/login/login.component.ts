3c1b1e791281c9f6320f509f75551391541bff64
Edwin Pinchao <epinchao@krugerschool.edu.ec>
2024-10-04 10:26:02 -0500

    " Create login , home, regiter component, use service regiter create form validation"

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,22 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { AuthService } from '../../services';
+import { MessageService } from 'primeng/api';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, CardModule, FormsModule ,PasswordModule, InputTextModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  login = {
+    email: '',
+    password: '',
+  };
+
+
   //private loginService: LoginService
   constructor(){}
 
-  login(form: NgForm){
-    const email = form.value.email;
-    const password = form.value.password;
-    // this.loginService.login(email, password);
-  }
+  onLogin(){
+    console.log(this.login)
 
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,18 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { RegisterPostData, User } from '../adapters/auth';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private readonly baseUrl='http://localhost:3002/auth/register';
+  private readonly baseUrl='http://localhost:3002/auth';
 
   http= inject(HttpClient)
 
   constructor() { }
 
-  registerUser(user: any) {
-    return this.http.post(this.baseUrl, user);
+
+
+  registerUser(postData: RegisterPostData) {
+    return this.http.post(`${this.baseUrl}/register`, postData);
+  }
+
+  getUserDetails(email: string, password: string): Observable<User[]> {
+    return this.http.get<User[]>(
+      `${this.baseUrl}/users?email=${email}&password=${password}`
+    );
   }
  
 
